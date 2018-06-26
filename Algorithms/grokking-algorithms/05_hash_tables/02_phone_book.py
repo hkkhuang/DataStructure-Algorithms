f1b9d45cc1ea9da2d4ddb47e0fe2c092d80ddfe7
hkkhuang <hkkhuang@163.com>
2018-06-26 21:06:34 +0800

    02_phone_book

---
+++ Algorithms/grokking-algorithms/05_hash_tables/02_phone_book.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Script Name   : 02_phone_book.py
+# Author        : huangkeke
+# Time          : 2018/6/26 14:54
+# Contact       : hkkhuang@163.com
+# Description	: 将散列表用于查找
+
+phone_book = dict()
+phone_book['Tom'] = 89689654
+phone_book['Mark'] = 89765487
+print(phone_book['Tom'])
+# output: 89689654
